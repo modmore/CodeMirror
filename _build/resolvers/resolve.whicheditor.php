f926b02a1249616ab333142a72cd40e56f9074de
Shaun McCormick <shaun@modxcms.com>
2010-07-14 14:43:13 -0500

    Now auto-assigns which_element_editor to CodeMirror

--- _build/resolvers/resolve.whicheditor.php
+++ _build/resolvers/resolve.whicheditor.php
@@ -10,22 +10,21 @@ if ($pluginid= $object->get('id')) {
     switch ($options[xPDOTransport::PACKAGE_ACTION]) {
         case xPDOTransport::ACTION_INSTALL:
         case xPDOTransport::ACTION_UPGRADE:
-            /*
-            $object->xpdo->log(xPDO::LOG_LEVEL_INFO,'Attempting to set which_editor setting to CodeMirror.');
-            $setting = $object->xpdo->getObject('modSystemSetting',array('key' => 'which_editor'));
+            $object->xpdo->log(xPDO::LOG_LEVEL_INFO,'Attempting to set which_element_editor setting to CodeMirror.');
+            $setting = $object->xpdo->getObject('modSystemSetting',array('key' => 'which_element_editor'));
             if ($setting) {
                 $setting->set('value','CodeMirror');
                 $setting->save();
+                unset($setting);
             }
-            unset($setting);
-            */
+            
             $object->xpdo->log(xPDO::LOG_LEVEL_INFO,'Attempting to set use_editor setting to on.');
             $setting = $object->xpdo->getObject('modSystemSetting',array('key' => 'use_editor'));
             if ($setting) {
                 $setting->set('value',1);
                 $setting->save();
+                unset($setting);
             }
-            unset($setting);
             break;
         case xPDOTransport::ACTION_UNINSTALL:
             $success= true;
