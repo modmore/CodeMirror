c129c5f91582992052748f44ef09b57e9cc79b5e
JP DeVries <johnpaul.devries@gmail.com>
2014-02-13 11:42:29 -0800

    Releasing 2.2.1-pl
    Closes #1

--- _build/build.transport.php
+++ _build/build.transport.php
@@ -15,7 +15,7 @@ set_time_limit(0);
 define('PKG_NAME','CodeMirror');
 define('PKG_NAMESPACE',strtolower(PKG_NAME));
 define('PKG_VERSION','2.2.1');
-define('PKG_RELEASE','beta');
+define('PKG_RELEASE','pl');
 
 /* define sources */
 $root = dirname(dirname(__FILE__)).'/';
